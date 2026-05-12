5f124693efcfd3a398610639e4debf0b3c1a6e5a
Jorge Alejandro Rodríguez Aldana <jorgealejandro1999@gmail.com>
2026-05-12 01:28:56 +0000

    Inline nvim config (was submodule)
    Removes the submodule indirection; nvim config is now tracked directly in this repo. The standalone JARA99/nvim-config repo on GitHub is preserved but no longer referenced.

---
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,22 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'alexghergh/nvim-tmux-navigation',
+    config = function()
+      require('nvim-tmux-navigation').setup {
+        disable_when_zoomed = true,
+        keybindings = {
+          left = '<C-h>',
+          down = '<C-j>',
+          up = '<C-k>',
+          right = '<C-l>',
+          last_active = '<C-\\>',
+          next = '<C-Space>',
+        },
+      }
+    end,
+  },
+}
